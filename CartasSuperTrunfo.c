56cf3fb087d5b8c34f74797cee506190e86cf55a
nycollas2773 <nycollasc238@gmail.com>
2025-05-13 18:30:45 +0000

    variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,58 @@
 #include <stdio.h>
+    
+    int main (){
 
-int main() {
-    printf("Desafio Super Trunfo - Países\n");
-}
+        char estado1;
+        char codigo1[];
+        char cidade1;
+        int populacao1;
+        float area1;
+        float pib1;
+        int pontos_turisticos1;
+
+        char estado2;
+        char codigo2[];
+        char cidade2[];
+        int populacao2;
+        float area2;
+        float pib2;
+        int pontos_turisticos2;
+         
+         // carta1; 
+
+    printf("Cadastro da primeira carta:\n");
+    printf("Digite a população:");
+    scanf("%d", &carta1.população);
+    printf("Digite a área:");
+    scranf("%f", &carta1.área);
+    printf("Digite o pib:");
+    scranf("%f", &carta1.pib);
+    printf("Digite o nome dos pontos_turísticos:")
+    scranf("%d", &carta1.pontos_turísticos);
+
+          // carta2;
+
+    printf("Cadastro da segunda carta:\n");
+    printf("Digite a população:");
+    scanf("%d", &carta2.população);
+    printf("Digite a área:");
+    scranf("%f", &carta2.área);
+    printf("Digite o pib:");
+    scranf("%f", &carta2.pib);
+    printf("Digite o nome dos pontos_turísticos:")
+    scranf("%d", &carta2.pontos_turísticos);
+
+    // Dados da carta 1
+
+    }
+
+
+   
+
+
+
+
+        
+
+
+
